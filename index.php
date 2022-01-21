38c647cd12a35e52ab8ae47ef32187150d10079d
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-21 11:41:23 +0100

    Exo 164 ok

--- index.php
+++ index.php
@@ -7,3 +7,48 @@
  * 3. Uploadez ce projet sur votre serveur et testez !
  */
 // TODO Votre code ici
+
+$server = [
+    $_SERVER['GATEWAY_INTERFACE'],
+    $_SERVER['SERVER_ADDR'],
+    $_SERVER['SERVER_NAME'],
+    $_SERVER['SERVER_SOFTWARE'],
+    $_SERVER['SERVER_PROTOCOL'],
+    $_SERVER['REQUEST_METHOD'],
+    $_SERVER['REQUEST_TIME'],
+    $_SERVER['REQUEST_TIME_FLOAT'],
+    $_SERVER['QUERY_STRING'],
+    $_SERVER['DOCUMENT_ROOT'],
+    $_SERVER['HTTP_ACCEPT'],
+    $_SERVER['HTTP_ACCEPT_CHARSET'],
+    $_SERVER['HTTP_ACCEPT_ENCODING'],
+    $_SERVER['HTTP_ACCEPT_LANGUAGE'],
+    $_SERVER['HTTP_CONNECTION'],
+    $_SERVER['HTTP_HOST'],
+    $_SERVER['HTTP_REFERER'],
+    $_SERVER['HTTP_USER_AGENT'],
+    $_SERVER['HTTPS'],
+    $_SERVER['REMOTE_ADDR'],
+    $_SERVER['REMOTE_HOST'],
+    $_SERVER['REMOTE_PORT'],
+    $_SERVER['REMOTE_USER'],
+    $_SERVER['REDIRECT_REMOTE_USER'],
+    $_SERVER['SCRIPT_FILENAME'],
+    $_SERVER['SERVER_ADMIN'],
+    $_SERVER['SERVER_PORT'],
+    $_SERVER['SERVER_SIGNATURE'],
+    $_SERVER['PATH_TRANSLATED'],
+    $_SERVER['SCRIPT_NAME'],
+    $_SERVER['REQUEST_URI'],
+    $_SERVER['PHP_AUTH_DIGEST'],
+    $_SERVER['PHP_AUTH_USER'],
+    $_SERVER['PHP_AUTH_PW'],
+    $_SERVER['AUTH_TYPE'],
+    $_SERVER['PATH_INFO'],
+    $_SERVER['ORIG_PATH_INFO'],
+];
+
+$count = count($server);
+for ($i = 0; $i < $server; $i++){
+    echo $server[$i];
+}
